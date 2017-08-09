e1d3444698d8ad4be658c593604df3cd275767fd
Guy Shaanan <guy.shaanan@nokia.com>
2017-08-09 15:44:11 +0300

    No more hard-coded elements height, everything is now flexbox

--- src/app/executions/executions.component.ts
+++ src/app/executions/executions.component.ts
@@ -4,7 +4,14 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'cf-executions',
-    template: `<router-outlet></router-outlet>`
+    template: `<router-outlet></router-outlet>`,
+    styles: [`
+        :host{
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+        }
+    `]
 })
 export class ExecutionsComponent {
 
