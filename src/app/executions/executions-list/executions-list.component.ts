1ccdb425db8530f5febdfbf7ab74470877aaaaa2
Guy Shaanan <guy.shaanan@nokia.com>
2017-08-14 09:24:47 +0300

    Add refresh button to executions list

--- src/app/executions/executions-list/executions-list.component.ts
+++ src/app/executions/executions-list/executions-list.component.ts
@@ -1,24 +1,51 @@
 // Copyright (C) 2017 Nokia
 
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {MistralService} from "../../engines/mistral/mistral.service";
 import {Execution} from "../../shared/models/execution";
+import {trigger, style, transition, animate, query, stagger} from '@angular/animations';
 
 @Component({
     selector: 'cf-executions-list',
     templateUrl: './executions-list.component.html',
-    styleUrls: ['./executions-list.component.scss']
+    styleUrls: ['./executions-list.component.scss'],
+    animations: [
+        trigger('listAnimation', [
+            transition('* => *', [
+                query(':enter', style({opacity: 0}), {optional: true}),
+                query(':enter', stagger('80ms', [
+                    animate('.15s ease-in', style({opacity: 1}))
+                ]), {optional: true})
+            ])
+        ])
+    ]
 })
 export class ExecutionsListComponent implements OnInit {
+    @ViewChild("executionsList") private executionsList: ElementRef;
     executions: Execution[] = [];
     search: string;
+    loading = false;
 
     constructor(private service: MistralService) {}
 
     ngOnInit() {
+        this.refresh();
+    }
+
+    refresh() {
+        this.loading = true;
         this.service.executions()
-            // only display "root" executions (that have no 'task_execution_id' value)
-            .subscribe(executions => this.executions = executions.filter(exec => !exec.task_execution_id));
+            .subscribe(
+                executions => {
+                    // only display "root" executions (that have no 'task_execution_id' value)
+                    this.executions = executions.filter(exec => !exec.task_execution_id);
+
+                    // scroll the list to top
+                    this.executionsList.nativeElement.scrollTop = 0;
+                },
+                () => {},
+                () => this.loading = false
+            );
     }
 
     execTrackBy(index: number, item: Execution) {
